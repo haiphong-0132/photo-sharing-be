e859074caf33366068a28abbe5b23833532597f9
CodeSandbox <support@codesandbox.io>
2024-05-03 14:04:05 +0000

    Sandbox snapshot commit

--- db/dbLoad.js
+++ db/dbLoad.js
@@ -7,6 +7,8 @@ const User = require("../db/userModel.js");
 const Photo = require("../db/photoModel.js");
 const SchemaInfo = require("../db/schemaInfo.js");
 
+const versionString = "1.0";
+
 async function dbLoad() {
   try {
     await mongoose.connect(process.env.DB_URL);
@@ -42,6 +44,56 @@ async function dbLoad() {
     } catch (error) {
       console.error("Error create user", error);
     }
+  }
+  const photoModels = [];
+  const userIDs = Object.keys(mapFakeId2RealId);
+  userIDs.forEach(function (id) {
+    photoModels.push(...models.photoOfUserModel(id));
+  });
+  for (const photo of photoModels) {
+    photoObj = await Photo.create({
+      file_name: photo.file_name,
+      date_time: photo.date_time,
+      user_id: mapFakeId2RealId[photo.user_id],
+    });
+    photo.objectID = photoObj._id;
+    if (photo.comments) {
+      photo.comments.forEach(function (comment) {
+        photoObj.comments = photoObj.comments.concat([
+          {
+            comment: comment.comment,
+            date_time: comment.date_time,
+            user_id: comment.user.objectID,
+          },
+        ]);
+        console.log(
+          "Adding comment of length %d by user %s to photo %s",
+          comment.comment.length,
+          comment.user.objectID,
+          photo.file_name,
+        );
+      });
+    }
+    try {
+      await photoObj.save();
+      console.log(
+        "Adding photo:",
+        photo.file_name,
+        " of user ID ",
+        photoObj.user_id,
+      );
+    } catch (error) {
+      console.error("Error create photo", error);
+    }
+  }
+
+  try {
+    schemaInfo = await SchemaInfo.create({
+      version: versionString,
+    });
+    console.log("SchemaInfo object created with version ", schemaInfo.version);
+  } catch (error) {
+    console.error("Error create schemaInfo", reportError);
   }
   mongoose.disconnect();
 }
